fedced7c4a2ee9e0cbadbfbe25b220dd4a164ddc
Byte-nana <nanadevelop44@gmail.com>
2025-09-28 22:30:35 +0700

    style(base-apparel): mobile-design

---
+++ base-apparel-coming-soon-master/src/main.js
@@ -0,0 +1,8 @@
+// email validation
+// 1. user input value
+// 2. check === email reg
+// 3. don't match
+// 3-1. input border color, show ! image, show under text
+
+const email = document.querySelector('#input__email');
+console.log(email.value);
