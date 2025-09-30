8a92de107a6c959b59d254361f125c4d4b61f448
Byte-nana <nanadevelop44@gmail.com>
2025-09-30 22:02:49 +0700

    feat(base-apparel): adding from event-ongoing

--- base-apparel-coming-soon-master/src/main.js
+++ base-apparel-coming-soon-master/src/main.js
@@ -4,5 +4,36 @@
 // 3. don't match
 // 3-1. input border color, show ! image, show under text
 
-const email = document.querySelector('#input__email');
-console.log(email.value);
+const formEmail = document.querySelector('.form__email');
+const inputEmail = document.querySelector('#input__email');
+const main = document.querySelector('.main');
+const errorImg = document.querySelector('.form__error-icon');
+
+formEmail.addEventListener('submit', (e) => {
+  let emailInput = inputEmail.value.trim();
+  const emailRegex = /[a-zA-Z0-9._+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9.]+/;
+  let error = main.querySelector('.error__message');
+
+  e.preventDefault();
+
+  if (emailInput === '' || !emailRegex.test(emailInput)) {
+    // error message
+    // todo : don't duplicate.
+    if (!error) {
+      createMessage();
+    }
+    // border colour
+    formEmail.classList.add('form__error');
+    // error icon
+    errorImg.style.display = 'block';
+  }
+  //input initalise
+  inputEmail.value = '';
+});
+
+function createMessage() {
+  const error = document.createElement('p');
+  error.setAttribute('class', 'error__message');
+  error.innerHTML = 'Please provide a valid email';
+  main.appendChild(error);
+}
