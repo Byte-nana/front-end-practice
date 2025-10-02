cea148e1f9118bb81609f885bd0c758354a59351
Byte-nana <nanadevelop44@gmail.com>
2025-10-03 00:26:48 +0900

    style(base-apparel): desktop layout

--- base-apparel-coming-soon-master/src/main.js
+++ base-apparel-coming-soon-master/src/main.js
@@ -1,8 +1,4 @@
 // email validation
-// 1. user input value
-// 2. check === email reg
-// 3. don't match
-// 3-1. input border color, show ! image, show under text
 
 const formEmail = document.querySelector('.form__email');
 const inputEmail = document.querySelector('#input__email');
@@ -10,25 +6,19 @@ const main = document.querySelector('.main');
 const errorImg = document.querySelector('.form__error-icon');
 
 formEmail.addEventListener('submit', (e) => {
-  let emailInput = inputEmail.value.trim();
+  let email = inputEmail.value.trim();
   const emailRegex = /[a-zA-Z0-9._+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9.]+/;
-  let error = main.querySelector('.error__message');
 
   e.preventDefault();
 
-  if (emailInput === '' || !emailRegex.test(emailInput)) {
-    // error message
-    // todo : don't duplicate.
-    if (!error) {
-      createMessage();
-    }
-    // border colour
-    formEmail.classList.add('form__error');
-    // error icon
-    errorImg.style.display = 'block';
+  if (email === '') {
+    isValid();
+    inputEmail.value = '';
+  } else if (!emailRegex.test(email)) {
+    isValid();
+  } else {
+    alert('Login successful');
   }
-  //input initalise
-  inputEmail.value = '';
 });
 
 function createMessage() {
@@ -36,4 +26,13 @@ function createMessage() {
   error.setAttribute('class', 'error__message');
   error.innerHTML = 'Please provide a valid email';
   main.appendChild(error);
+}
+
+function isValid() {
+  let errorMessage = main.querySelector('.error__message');
+  if (!errorMessage) {
+    createMessage();
+  }
+  formEmail.classList.add('form__error');
+  errorImg.style.display = 'block';
 }
