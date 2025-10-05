ceb0efecf7aeba836ef9c06ba8e8aac002d46732
Byte-nana <nanadevelop44@gmail.com>
2025-10-06 01:44:15 +0900

    feat(article-preview):reuse .share-btn

--- article-preview-component-master/src/main.js
+++ article-preview-component-master/src/main.js
@@ -3,12 +3,8 @@
 //Show share-Links
 const shareBtn = document.querySelector('.profile__btn');
 const shareBox = document.querySelector('.share-box');
-const closeBtn = document.querySelector('.share__btn');
 
 shareBtn.addEventListener('click', () => {
-  shareBox.classList.add('open');
-});
-
-closeBtn.addEventListener('click', () => {
-  shareBox.classList.remove('open');
+  shareBox.classList.toggle('open');
+  shareBtn.classList.toggle('active');
 });
