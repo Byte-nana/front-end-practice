88301dabd34d757dd6f63ee08bf8b7aa7747314c
Byte-nana <nanadevelop44@gmail.com>
2025-11-01 20:38:22 +0000

    feat:nav menu toggle event

--- news-homepage-main/src/main.js
+++ news-homepage-main/src/main.js
@@ -1,5 +1,20 @@
 'use strict';
 
-// menu bar toggle
-// change the layout of the navbar
-// desiapper button X, menu icon and change the layout of the nav
+const navToggle = document.querySelector('.news__header');
+const headerNav = document.querySelector('.header__nav');
+const body = document.querySelector('body');
+
+navToggle.addEventListener('click', (e) => {
+  toggleNav(e.target, '.header__menubar img', 'add');
+});
+
+navToggle.addEventListener('click', (e) => {
+  toggleNav(e.target, '.nav__close-btn img', 'remove');
+});
+
+function toggleNav(target, className, state) {
+  if (target.matches(className)) {
+    headerNav.classList[state]('open');
+    body.classList[state]('overlay');
+  }
+}
